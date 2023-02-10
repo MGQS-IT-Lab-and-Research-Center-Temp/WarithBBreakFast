baa7f6cbdef6b2744be910e4c858cf1a36bf602a
warith23 <ismailayomide32@gmail.com>
2023-02-10 20:27:13 +0100

    first commit

---
+++ Context/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BuberBreakfast.Entities;
+
+namespace BuberBreakfast.Context
+{
+    public class ApplicationContext : DbContext
+    {
+       public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) 
+       {
+
+       }
+         public DbSet<BreakFast> BreakFasts { get; set; }
+
+        
+    }
+}
+ 
---
+++ Controllers/BreakFastController.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BuberBreakfast.Contracts.Service;
+using BuberBreakfast.Entities;
+using BuberBreakfast.DTO;
+
+namespace BuberBreakfast.Controllers
+{
+    //[Route("[controller]")]
+    public class BreakFastController : Controller
+      {
+        
+        private readonly IBreakFastService _breakFastService;
+
+        public BreakFastController(IBreakFastService breakfastService) => _breakFastService = breakfastService;
+
+        public IActionResult Index()
+        {
+            var breakfasts = _breakFastService.PrintAllBreakFast();
+            return View(breakfasts.Data);
+        }
+ 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(BreakFastDto request)
+        {
+            var create = _breakFastService.RegisterBreakFast(request);
+            if (create.Status == false)
+            {
+                ViewBag.Message = create.Message;
+                return View();
+            }
+            return RedirectToAction("Index" , "Home");
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Update(UpdateBreakFastDto updateBreakFastDto, int id)
+        {
+            var breakfast = _breakFastService.UpdateBreakFast(id, updateBreakFastDto);
+            if (breakfast.Status == false)
+            {
+                ViewBag.Message = breakfast.Message;
+                return View();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var breakfast = _breakFastService.GetBreakFast(id);
+            return View(breakfast);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteBreakFast(int id)
+        {
+            var breakfast = _breakFastService.DeleteBreakFast(id);
+            if (breakfast.Status == false)
+            {
+                ViewBag.Message = breakfast.Message;
+                return View();
+            }
+            return RedirectToAction("Index");
+        }
+        
+        public IActionResult Details(int id)
+        {
+            var breakfast = _breakFastService.GetBreakFast(id);
+            if (breakfast.Status == false)
+            {
+                ViewBag.Message = breakfast.Message;
+                return View();
+            }
+            return View(breakfast.Data);
+        }
+    }
+} 
